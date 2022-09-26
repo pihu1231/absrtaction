a8861687e5fff1d72956415966d61829ac70328a
pihu1231 <93186329+pihu1231@users.noreply.github.com>
2022-09-26 12:14:31 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,8 @@
+package CoreJava;
+
+public abstract class Abstraction {
+	//method of abstract class
+public void display(){
+	System.out.println("fhfhhx");
+}
+}
---
+++ Extract.java
@@ -0,0 +1,12 @@
+package CoreJava;
+
+ class Extract  extends Abstraction{
+	 
+	 public static void main(String[] args) {
+		//creating an obj of subclass
+		 Extract e = new  Extract ();
+		 //access method of abstract class using obj of sub class
+		 e.display();
+	 }
+
+}
